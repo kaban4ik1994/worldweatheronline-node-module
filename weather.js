358cacb2f0faad08bb3a7d006712cbed583dbd80
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-04-17 23:43:02 +0300

    Params in the main function was modified.

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
 var baseUrlApiWeather = 'http://api.worldweatheronline.com/free/v2/weather.ashx';
 var request = require('request');
 
-var weather = function (options) {
+var weather = function (options, callbackFunction) {
     var requestParams = {
         key: options.key,
         format: 'json',
-        num_of_days: 1,
+        num_of_days: options.num_of_days | 1,
         q: options.q,
         date: options.date
     };
 
-    request({ url: baseUrlApiWeather, qs: requestParams }, options.callbackFunction);
+    request({ url: baseUrlApiWeather, qs: requestParams }, callbackFunction);
 };
 
 module.exports = weather;
